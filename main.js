484c1dbdc93282a8a2698fe683c6b16bc398030e
ivanisovmaks <166316568+ivanisovmaks@users.noreply.github.com>
2024-10-04 12:36:36 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-
 const character = createCharacter('Pikachu', 100, 'health-character', 'progressbar-character');
 const enemy1 = createCharacter('Charmander', 100, 'health-enemy1', 'progressbar-enemy1');
 const enemy2 = createCharacter('Squirtle', 100, 'health-enemy2', 'progressbar-enemy2');
-
 
 const $btnKick = document.getElementById('btn-kick');
 const $btnStrongKick = document.getElementById('btn-strong-kick');
@@ -13,64 +11,57 @@ function createCharacter(name, hp, hpElementId, progressbarElementId) {
         defaultHP: hp,
         damageHP: hp,
         elHP: document.getElementById(hpElementId),
-        elProgressbar: document.getElementById(progressbarElementId)
+        elProgressbar: document.getElementById(progressbarElementId),
+        
+        renderHP: function() {
+            this.renderHPLife();
+            this.renderProgressbarHP();
+        },
+        
+        renderHPLife: function() {
+            this.elHP.innerText = `${this.damageHP} / ${this.defaultHP}`;
+        },
+
+        renderProgressbarHP: function() {
+            const percentage = (this.damageHP / this.defaultHP) * 100;
+            this.elProgressbar.style.width = `${percentage}%`;
+
+            if (percentage < 30) {
+                this.elProgressbar.style.background = 'red';
+            } else if (percentage < 60) {
+                this.elProgressbar.style.background = 'yellow';
+            } else {
+                this.elProgressbar.style.background = 'lime';
+            }
+        },
+
+        changeHP: function(damage) {
+            if (this.damageHP <= damage) {
+                this.damageHP = 0;
+                alert(`Бідний ${this.name} програв бій!`);
+                disableButtons();
+            } else {
+                this.damageHP -= damage;
+            }
+            this.renderHP();
+        }
     };
 }
 
-
 function init() {
     console.log('Start Game!');
-    renderHP(character);
-    renderHP(enemy1);
-    renderHP(enemy2);
+    character.renderHP();
+    enemy1.renderHP();
+    enemy2.renderHP();
 }
-
-
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressbarHP(person);
-}
-
-
-function renderHPLife(person) {
-    person.elHP.innerText = `${person.damageHP} / ${person.defaultHP}`;
-}
-
-function renderProgressbarHP(person) {
-    const percentage = (person.damageHP / person.defaultHP) * 100;
-    person.elProgressbar.style.width = `${percentage}%`;
-
-
-    if (percentage < 30) {
-        person.elProgressbar.style.background = 'red';
-    } else if (percentage < 60) {
-        person.elProgressbar.style.background = 'yellow';
-    } else {
-        person.elProgressbar.style.background = 'lime';
-    }
-}
-
-
-function changeHP(person, damage) {
-    if (person.damageHP <= damage) {
-        person.damageHP = 0;
-        alert(`Бідний ${person.name} програв бій!`);
-        disableButtons();
-    } else {
-        person.damageHP -= damage;
-    }
-    renderHP(person); 
-}
-
 
 function randomDamage(max) {
     return Math.ceil(Math.random() * max);
 }
 
-
 function attack(damageFunction) {
-    changeHP(enemy1, damageFunction());
-    changeHP(enemy2, damageFunction());
+    enemy1.changeHP(damageFunction());
+    enemy2.changeHP(damageFunction());
 }
 
 $btnKick.addEventListener('click', function () {
