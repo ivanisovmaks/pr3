30b39a8c3925693cbb0f3b64adf22e3710fab8da
ivanisovmaks <166316568+ivanisovmaks@users.noreply.github.com>
2024-10-25 13:45:23 +0300

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -1,24 +1,37 @@
+import { getRandomLog } from './logs.js';
+
 export class Pokemon {
-    constructor(name, health, progressBar, healthText) {
+    constructor(name, health, progressBarId, healthTextId, addLog) {
         this.name = name;
         this.health = health;
         this.maxHealth = health;
-        this.progressBar = progressBar;
-        this.healthText = healthText;
+        this.progressBar = document.getElementById(progressBarId);
+        this.healthText = document.getElementById(healthTextId);
+        this.addLog = addLog;
         this.updateHealthBar();
     }
 
-    updateHealthBar() {
-        this.health = Math.max(this.health, 0);
-        const healthPercentage = (this.health / this.maxHealth) * 100;
-        this.progressBar.style.width = `${healthPercentage}%`;
-        this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
-    }
+		updateHealthBar() {
+			this.health = Math.max(this.health, 0);
+			const healthPercentage = (this.health / this.maxHealth) * 100;
+			this.progressBar.style.width = `${healthPercentage}%`;
+			this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
+			this.changeHealthColor();
+		}
 
-    receiveDamage(damage, enemyName, getRandomLog, addLog) {
+		changeHealthColor() {
+			this.progressBar.classList.remove('low', 'critical');
+			if (this.health <= 20) {
+				this.progressBar.classList.add('critical');
+			} else if (this.health <= 60) {
+				this.progressBar.classList.add('low');
+			}
+		}
+
+    receiveDamage(damage, enemyName) {
         this.health -= damage;
         this.updateHealthBar();
         const logMessage = getRandomLog(this.name, enemyName);
-        addLog(`${logMessage} ${this.name} отримав ${damage} пошкоджень. Залишилось ${this.health} HP.`);
+        this.addLog(`${logMessage} ${this.name} отримав ${damage} пошкоджень. Залишилось ${this.health} HP.`);
     }
 }
